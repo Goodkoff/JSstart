d726beb1ad39b226d12665e13a94a513f50485e9
Anton Gudkov <a.gudkov@planeta-kino.kiev.ua>
2016-05-23 16:54:44 +0300

    Start learning JS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+//HomeWork 1;
+alert('I am Javascript');
+
+//HomeWork 2;
+var admin, name;
+name = 'Vasiliy';
+admin = name;
+console.log (admin);
+
+//HomeWork 3;
+var planetName = 'Earth';
+var userName = 'Peter';
